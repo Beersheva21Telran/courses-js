e8e6dfeea297cadc3e27bf67fcf5cd70f4a0d223
Yuragranovsky <yuragranovsky@gmail.com>
2021-12-21 11:54:31 +0200

    intermediate before break commit

--- src/main.js
+++ src/main.js
@@ -8,48 +8,53 @@ import createCourse from "./models/Course";
 import FormHandler from "./ui/form-handler";
 import TableHandler from "./ui/table-handler";
 const N_RANDOM_COURSES = 5;
+//Creating required objects
 const colledge = new Colledge(courseProvider, courseData);
-window.removeCourse = function (id) {
-    if (confirm(`you are going to remove course with id=${id}`)) {
-        colledge.removeCourseById(id);
-        tableCourses.removeRow(id);
-    }
-}
-createRandomCourses();
-
-function createRandomCourses() {
-    const { minCost, maxCost, minHours, maxHours, minYear, maxYear, courseNames, lecturers, types, timing } = { ...courseData };
-    for (let i = 0; i < N_RANDOM_COURSES; i++) {
-        const name = courseNames[getRandomInteger(0, courseNames.length - 1)];
-        const lecture = lecturers[getRandomInteger(0, lecturers.length - 1)];
-        const hours = getRandomInteger(minHours, maxHours);
-        const cost = getRandomInteger(minCost, maxCost);
-        const type = getRandomElement(types);
-        const dayEveningId = getRandomInteger(0, 2);
-        const dayEvening = dayEveningId < 2 ? [timing[dayEveningId]] : timing;
-        const startDate = getRandomDate(minYear, maxYear);
-        const course = createCourse(name, lecture, hours, cost, type, dayEvening, startDate);
-        colledge.addCourse(course);
-    }
-}
-
- const formCourse = new FormHandler("course-form","alert-place");
-const coursesSort = function (key) {
-    tableCourses.clear();
-    colledge.sort(key).forEach(c => tableCourses.addRow(c, c.id));
-}
+const formCourse = new FormHandler("course-form", "alert-place");
+const formCostStatistics = new FormHandler("cost-statistics-form");
+const formHoursStatistics = new FormHandler("hours-statistics-form");
 const tableCourses = new TableHandler("courses-header", "courses-body",
     ["id", "courseName", "lecturerName", "hours", "cost", "openDate"], coursesSort, "removeCourse");
-debugDisplayColledge();
-debugDisplayColledge();
-
-
-FormHandler.fillOptions("course-name", courseData.courseNames);
-FormHandler.fillOptions("lecturer-name", courseData.lecturers);
-formCourse.addHandler(course => {
-    colledge.addCourse(course);
-    tableCourses.addRow(course, course.id);
-})
+const tableIntervalcost = new TableHandler("interval-cost-header", "interval-cost-body",
+    ["minInterval", "maxInterval", "amount"]);
+const tableIntervalHours = new TableHandler("interval-hours-header", "interval-hours-body",
+    ["minInterval", "maxInterval", "amount"]);
+/****************************************************************** */
+//functions
+async function createRandomCourses() {
+    const { minCost, maxCost, minHours, maxHours, minYear, maxYear, courseNames, lecturers, types, timing } = { ...courseData };
+    for (let i = 0; i < N_RANDOM_COURSES; i++) {
+        await createCourse(courseNames, lecturers, minHours, maxHours, minCost, maxCost, types, timing, minYear, maxYear);
+    }
+}
+window.removeCourse = async function (id) {
+    if (confirm(`you are going to remove course with id=${id}`)) {
+        try {
+            await colledge.removeCourseById(id);
+             tableCourses.removeRow(id);
+        } catch (err) {
+            //TODO Alert functionality
+        }
+       
+    }
+}
+const coursesSort = async function (key) {
+    tableCourses.clear();
+    const sortedColledge = await colledge.sort(key);
+    sortedColledge.forEach(c => tableCourses.addRow(c, c.id));
+}
+async function createCourse(courseNames, lecturers, minHours, maxHours, minCost, maxCost, types, timing, minYear, maxYear) {
+    const name = courseNames[getRandomInteger(0, courseNames.length - 1)];
+    const lecture = lecturers[getRandomInteger(0, lecturers.length - 1)];
+    const hours = getRandomInteger(minHours, maxHours);
+    const cost = getRandomInteger(minCost, maxCost);
+    const type = getRandomElement(types);
+    const dayEveningId = getRandomInteger(0, 2);
+    const dayEvening = dayEveningId < 2 ? [timing[dayEveningId]] : timing;
+    const startDate = getRandomDate(minYear, maxYear);
+    const course = createCourse(name, lecture, hours, cost, type, dayEvening, startDate);
+    await colledge.addCourse(course);
+}
 
 function debugDisplayColledge() {
 
@@ -59,28 +64,40 @@ function debugDisplayColledge() {
 
     });
 }
-const getIntervalHours = function (interval) {
+const getIntervalHours = async function (interval) {
     tableIntervalHours.clear();
-    let arr = colledge.getElementsByHours(interval);
+    let arr = await colledge.getElementsByHours(interval);
     arr.forEach(c => tableIntervalHours.addRow(c));
 }
-
-const formHoursStatistics = new FormHandler("hours-statistics-form");
-FormHandler.fillOptions("select-hours", courseData.hoursDivider);
-formHoursStatistics.addHandler(getIntervalHours);
-const tableIntervalHours = new TableHandler("interval-hours-header", "interval-hours-body", ["minInterval", "maxInterval", "amount"]);
-
-
-const getIntervalCost = function (interval) {
+const getIntervalCost = async function (interval) {
     tableIntervalcost.clear();
-    let arr = colledge.getElementsByCost(interval);
+    let arr = await colledge.getElementsByCost(interval);
     arr.forEach(c => tableIntervalcost.addRow(c));
 }
+/****************************************************************/
+// Actions
+createRandomCourses();
+FormHandler.fillOptions("course-name", courseData.courseNames);
+FormHandler.fillOptions("lecturer-name", courseData.lecturers);
+formCourse.addHandler(course => {
+    colledge.addCourse(course);
+    tableCourses.addRow(course, course.id);
+})
 
-const formCostStatistics = new FormHandler("cost-statistics-form");
+
+
+
+FormHandler.fillOptions("select-hours", courseData.hoursDivider);
+formHoursStatistics.addHandler(getIntervalHours);
+
+
+
+
+
+
 FormHandler.fillOptions("select-cost", courseData.costDivider);
 formCostStatistics.addHandler(getIntervalCost);
-const tableIntervalcost = new TableHandler("interval-cost-header", "interval-cost-body", ["minInterval", "maxInterval", "amount"]);
+
 
 
 
